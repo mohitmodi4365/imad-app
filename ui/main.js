1f72e79acd93624fc545e8f582bd2fda7b415fab
Mohit Modi <30765618+mohitmodi4365@users.noreply.github.com>
2017-08-07 20:19:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 //move
-var img;
-img=document.getElementByID('madi');
-img.onclick= function() {
+var img= document.getElementById('madi');
+
+img.onclick = function() {
     img.style.marginleft='100px';
-}
+};
