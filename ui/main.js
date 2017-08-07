6640bdce42c58b444228dd954293bd7eb075a8ed
Mohit Modi <30765618+mohitmodi4365@users.noreply.github.com>
2017-08-07 20:51:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,14 +4,9 @@ var img= document.getElementById('madi');
 var marginLeft=0;
 function moveRight() {
      marginLeft = marginLeft + 10;
-     if(marginLeft <== 500px)
-     {
+     
     img.style.marginLeft = marginLeft + 'px';}
-    else
-    {
-        alert('bas have');
-    }
-}
+  
 
 img.onclick = function() {
     var interval = setInterval(moveRight, 100);
