fc509958ffec9241f502b29d9285a63ffe2231b7
Mohit Modi <30765618+mohitmodi4365@users.noreply.github.com>
2017-08-12 17:16:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ app.get('/', function (req, res) {
 app.get('/ui/contectus', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'contectus.html'));
 });
-
+var counter = 0;
+app.get('/counter', function(req, res)  {
+   counter=counter + 1;
+    res.send(counter.toString());
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
