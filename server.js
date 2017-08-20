c037ecb369b2c664f9551f95e71bd4fece03483e
Mohit Modi <30765618+mohitmodi4365@users.noreply.github.com>
2017-08-20 12:53:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config = {
     user: 'mohitmodi4365',
     database: 'mohitmodi4365',
     host: 'db.imad.hasura-app.io',
-    port: '5432',
+    port: '80',
     password: process.env.DB_PASSWORD
 };
 var app = express();
