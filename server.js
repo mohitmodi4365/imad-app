40782b57b382d6ea813faf3ac50ebc15ce4fbc8e
Mohit Modi <30765618+mohitmodi4365@users.noreply.github.com>
2017-08-20 12:47:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = reqire('pg').Pool;
 
+var config = {
+    user: 'mohitmodi4365',
+    database: 'mohitmodi4365',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool = new Pool(config);
+app.get('/test', function (req, res){
+   //make a select request
+   //return a response with results
+   pool.query('SELECT * FROM test',function(err, result){
+      if(err) {
+          res.status(50).send(err.toString());
+      } else {
+          res.send(JSON.stringyfy(result));
+      }
+   });
+});
 app.get('/ui/contectus', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'contectus.html'));
 });
